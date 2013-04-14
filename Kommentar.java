b41b137aa8f1bfadfbfd51cee12f3df2a4c1242c
Arzu Cevik <meinemail@zoho.com>
2013-04-15 01:19:42 +0300

    Kommentare

---
+++ Kommentar.java
@@ -0,0 +1,92 @@
+package Aufgabe4;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java-Klasse für anonymous complex type.
+ * 
+ * <p>Das folgende Schemafragment gibt den erwarteten Content an, der in dieser Klasse enthalten ist.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="Autor" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="Nachricht" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "autor",
+    "nachricht"
+})
+@XmlRootElement(name = "Kommentar")
+public class Kommentar {
+
+    @XmlElement(name = "Autor", required = true)
+    protected String autor;
+    @XmlElement(name = "Nachricht", required = true)
+    protected String nachricht;
+
+    /**
+     * Ruft den Wert der autor-Eigenschaft ab.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getAutor() {
+        return autor;
+    }
+
+    /**
+     * Legt den Wert der autor-Eigenschaft fest.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setAutor(String value) {
+        this.autor = value;
+    }
+
+    /**
+     * Ruft den Wert der nachricht-Eigenschaft ab.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getNachricht() {
+        return nachricht;
+    }
+
+    /**
+     * Legt den Wert der nachricht-Eigenschaft fest.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setNachricht(String value) {
+        this.nachricht = value;
+    }
+
+
+}
